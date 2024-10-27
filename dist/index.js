4b3930680073ca3cbd6aba5b6804d1107ee10ee6
Marcos <marcospradofelix@gmail.com>
2024-10-26 22:52:47 -0300

    feat: Add Express server setup
    Replaced "Hello, world!" console log with a basic Express server setup. Included middleware for JSON and URL-encoded data handling, static file setup, and view configuration. Introduced ClienteController and ClienteService to manage client routes.

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,20 @@
 "use strict";
-console.log("Hello, world!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const ExpressStaticSetUp_1 = __importDefault(require("./controller/config/ExpressStaticSetUp"));
+const ViewConfig_1 = __importDefault(require("./controller/config/ViewConfig"));
+const ClienteController_1 = require("./controller/ClienteController");
+const ClienteService_1 = require("./service/ClienteService");
+const app = (0, express_1.default)();
+const PORT = 3000;
+app.use(express_1.default.json());
+app.use(ExpressStaticSetUp_1.default);
+app.use(express_1.default.urlencoded({ extended: true }));
+(0, ViewConfig_1.default)(app);
+new ClienteController_1.ClienteController(new ClienteService_1.ClienteService(), app);
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
