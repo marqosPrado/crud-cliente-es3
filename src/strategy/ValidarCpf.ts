af5bd996f780fbe558e562e0cdc70342ecab84a1
Marcos <marcospradofelix@gmail.com>
2024-10-26 23:37:34 -0300

    feat: Refactor validation exceptions in ValidarCpf and ValidarEmail.
    Replaced generic error messages with specific exception classes to better handle and distinguish CPF and email validation errors. This ensures clearer error handling, improving code readability and maintainability.

--- src/strategy/ValidarCpf.ts
+++ src/strategy/ValidarCpf.ts
@@ -1,16 +1,18 @@
 import {Validar} from "./Validar";
 import {Cliente} from "../domain/cliente/Cliente";
+import {CpfNecessarioException} from "../domain/cliente/exceptions/cpf/CpfNecessarioException";
+import {CpfInvalidoException} from "../domain/cliente/exceptions/cpf/CpfInvalidoException";
 
 export class ValidarCpf implements Validar {
   processar(cliente: Cliente): void {
     const cpf: string = cliente.cpf;
     if (!cpf) {
-      throw new Error("CPF precisa ser informado");
+      throw new CpfNecessarioException();
     }
     const replacedCpf = cpf.replace(/[.\-]/g, "").trim();
     const cpfRegex = /^\d{11}$/gm;
     if (!cpfRegex.test(replacedCpf)) {
-      throw new Error("CPF inválido");
+      throw new CpfInvalidoException();
     }
   }
 }
--- src/strategy/ValidarEmail.ts
+++ src/strategy/ValidarEmail.ts
@@ -1,15 +1,17 @@
 import {Validar} from "./Validar";
 import {Cliente} from "../domain/cliente/Cliente";
+import {EmailNecessarioException} from "../domain/cliente/exceptions/email/EmailNecessarioException";
+import {EmailFormatoException} from "../domain/cliente/exceptions/email/EmailFormatoException";
 
 export class ValidarEmail implements Validar {
   processar(cliente: Cliente): void {
     const email: string = cliente.email;
     if (!email) {
-      throw new Error("E-mail precisa ser informado");
+      throw new EmailNecessarioException();
     }
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/gm;
     if (!emailRegex.test(email)) {
-      throw new Error("E-mail inválido");
+      throw new EmailFormatoException();
     }
   }
 }
