26cb4888ca2c983180f12347f9cfb2daea45b5c6
Marcos <marcospradofelix@gmail.com>
2024-11-09 21:34:37 -0300

    feat: Add validation listeners for CPF, birthdate, and name inputs
    - Expanded `validators.js` with new functions to validate CPF and birthdate formats.
    - Updated `eventListenners.js`:
      - Added listeners for CPF and birthdate inputs to format and validate user input dynamically.
      - Introduced name input listener to ensure non-empty validation.
    - Modified `main.js` to initialize listeners for name, CPF, birthdate, and email fields on page load.

--- public/js/inputs/eventListenners/eventListenners.js
+++ public/js/inputs/eventListenners/eventListenners.js
@@ -1,14 +1,84 @@
-import {validateEmail} from "../validators/validators.js";
+import {validateBirthDate, validateCpf, validateEmail} from "../validators/validators.js";
 
 export function setupEmailListener() {
     const emailInput = document.getElementById('email-input');
-    emailInput.addEventListener('blur', (e) => {
-        e.preventDefault()
+    emailInput.addEventListener('blur', () => {
         const isValid = emailInput.value;
         if (validateEmail(isValid)) {
             emailInput.classList.remove('invalid');
         } else {
             emailInput.classList.add('invalid');
+        }
+    })
+}
+
+export function setupCpfListener() {
+    const cpfInput = document.getElementById('cpf-input');
+    cpfInput.addEventListener('input', () => {
+        let cpf = cpfInput.value;
+
+        cpf = cpf.replace(/\D/g, '');
+        cpf = cpf.slice(0, 11);
+
+        if (cpf.length > 9) {
+            cpf = cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{1,2})/, '$1.$2.$3-$4');
+        } else if (cpf.length > 6) {
+            cpf = cpf.replace(/(\d{3})(\d{3})(\d{1,3})/, '$1.$2.$3');
+        } else if (cpf.length > 3) {
+            cpf = cpf.replace(/(\d{3})(\d{1,3})/, '$1.$2');
+        }
+        cpfInput.value = cpf;
+    })
+
+    cpfInput.addEventListener('blur', () => {
+        if (cpfInput.value.length === 0) {
+            cpfInput.classList.add('invalid');
+        } else {
+            cpfInput.classList.remove('invalid')
+        }
+        validateCpf(cpfInput.value) ? cpfInput.classList.remove('invalid') : cpfInput.classList.add('invalid');
+    })
+}
+
+export function setupBirthDateListener() {
+    const birthDateInput = document.getElementById('data-nascimento-input');
+    birthDateInput.addEventListener('input', () => {
+        let birthDate = birthDateInput.value;
+
+        birthDate = birthDate.replace(/\D/g, '');
+        birthDate = birthDate.slice(0, 8);
+
+        if (birthDate.length > 8) {
+            birthDate = birthDate.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
+        } else if (birthDate.length > 4) {
+            birthDate = birthDate.replace(/(\d{2})(\d{2})/, '$1/$2/');
+        } else if (birthDate.length > 2) {
+            birthDate = birthDate.replace(/(\d{2})/, '$1/');
+        }
+        birthDateInput.value = birthDate;
+    })
+
+    birthDateInput.addEventListener('blur', () => {
+        const birthDate = birthDateInput.value
+        if (birthDate.length === '') {
+            console.log("entrou no if data de nascimento")
+            birthDateInput.classList.add('invalid');
+        } else {
+            birthDateInput.classList.remove('invalid')
+        }
+        console.log("blur birthDate")
+        validateBirthDate(birthDate) ? birthDateInput.classList.remove('invalid') : birthDateInput.classList.add('invalid');
+    })
+}
+
+export function setupNameListener() {
+    const nameInput = document.getElementById('nome-input');
+    nameInput.addEventListener('blur', () => {
+        const name = nameInput.value;
+        if (name.length === 0) {
+            nameInput.classList.add('invalid');
+        } else {
+            nameInput.classList.remove('invalid');
         }
     })
 }
--- public/js/inputs/main.js
+++ public/js/inputs/main.js
@@ -1,7 +1,13 @@
-import {setupEmailListener} from "./eventListenners/eventListenners.js";
+import {
+    setupBirthDateListener,
+    setupCpfListener,
+    setupEmailListener,
+    setupNameListener
+} from "./eventListenners/eventListenners.js";
 
 document.addEventListener('DOMContentLoaded', () => {
-    const emailInput = document.getElementById('email-input');
-
-    setupEmailListener(emailInput);
+    setupNameListener();
+    setupCpfListener();
+    setupBirthDateListener();
+    setupEmailListener();
 })
